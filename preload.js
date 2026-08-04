59778024f9ac45d329aeb5a4c31d4355601afe37
Jimmy Al Ma <jimmyalma2121@gmail.com>
2026-08-04 18:21:13 +0700

    feat(preload): expose getSystemInfo via contextBridge

--- preload.js
+++ preload.js
@@ -1,5 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('api', {
-    getNodeVersion: () => ipcRenderer.invoke('get-node-version')
+    getNodeVersion: () => ipcRenderer.invoke('get-node-version'),
+    getSystemInfo: () => ipcRenderer.invoke('get-system-info')
 })
