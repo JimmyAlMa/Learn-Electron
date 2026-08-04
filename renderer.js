3a72a5983796147966a7b40e0c151dfd43539ce7
Jimmy Al Ma <jimmyalma2121@gmail.com>
2026-08-04 18:45:41 +0700

    feat: display system info on button click

--- renderer.js
+++ renderer.js
@@ -1,8 +1,23 @@
 const firstButton = document.querySelector('#firstButton')
-const firstResult = document.querySelector('#firstResult')
+const firstResult = document.querySelector('#firstResult') // Text
 
 firstButton.addEventListener('click', async () => {
     const version = await window.api.getNodeVersion()
 
     firstResult.innerText = version
+})
+
+const secondButton = document.querySelector('#secondButton')
+const secondResult = document.querySelector('#secondResult') // List
+
+secondButton.addEventListener('click', async () => {
+    const data = await window.api.getSystemInfo()
+    
+    secondResult.innerHTML = ''
+
+    Object.entries(data).forEach(([key, value]) => {
+        const li = document.createElement('li')
+        li.textContent = `${key}: ${value}`
+        secondResult.appendChild(li)
+    })
 })
